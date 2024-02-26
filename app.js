4be636a0320e861141cbcc6d192274d0fcf0ebd6
lovef2eLia <lia.lovef2e@gmail.com>
2024-02-26 21:54:57 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.get('/movies/:id', (req, res) => {
   const id = req.params.id;
   res.send(`read movie: ${id}`);
 });
+
+app.use(express.static('public'));
+
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
 });
